67cc19fa4fff235d3f73035e35eb4f62f3276169
silverrush2013 <silverrush2013@gmail.com>
2015-11-12 21:41:37 +0300

    new file:   Ekz5.pas

---
+++ Ekz5.pas
@@ -0,0 +1,58 @@
+program ekz5;
+
+const
+  n = 3 ;
+
+type
+  matrix = array[1..n, 1..n] of integer;
+
+var
+  a: matrix;
+  max: integer;
+
+procedure readmatrix;
+var
+  i, j: integer;
+begin
+  writeln('Введите все элементы матрицы через пробел');
+  for i := 1 to n do
+  begin
+    for j := 1 to n do
+    begin
+      read(a[i, j]);
+    end;
+  end;
+end;
+
+procedure magicsquare;
+var
+  j, i, sumdiag1, sumdiag2, s1, s2: integer;
+  proverka: boolean;
+begin
+  sumdiag1 := 0;
+  proverka:=true;
+  for j := 1 to n do
+    sumdiag1 := sumdiag1 + a[j, j];{Нашли сумму всех элементов первой диагонали}
+  for i := 1 to n do
+  begin
+    s1 := 0;
+    s2 := 0;
+    for j := 1 to n do 
+    begin
+      s1 := s1 + a[i, j];
+      s2 := s2 + a[j, i];
+    end;
+    if (s1 <> sumdiag1) or (s2 <> sumdiag1) then proverka := false;
+  end;
+  sumdiag2 := 0;
+  for i := 1 to n do
+    sumdiag2 := sumdiag2 + a[i, n - i + 1];
+  if sumdiag1 <> sumdiag2 then proverka := false;
+  if (proverka = true) then writeln('Матрица является магическим квадратом')
+  else writeln('Матрица не является магическим квадратом');
+end;
+
+begin
+  readmatrix;
+  magicsquare;
+end.
