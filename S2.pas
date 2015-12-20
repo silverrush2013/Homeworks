4488af99efbb112bb91b1f2d407ae2afbe286c00
silverrush2013 <silverrush2013@gmail.com>
2015-12-20 14:45:21 +0300

    modified:   S2.pas

--- S2.pas
+++ S2.pas
@@ -5,7 +5,14 @@ type
 
 var
   a: mas;
-  N, num1, num2, num3: integer;
+  N, num1, num2, num3, count1, count2,count3, i: integer;
+
+function DoesContain(a: mas; num: integer; N: integer): boolean;
+begin
+  if a[num - 1] = true then DoesContain := true
+  else DoesContain := false;
+end;
+
 
 procedure add(a: mas; num: integer; N: integer);
 begin
@@ -15,26 +22,38 @@ end;
 
 procedure Delete(a: mas; num: integer; n: integer);
 begin
-  a[num - 1] := false;
+  if DoesContain(a, num, N) = true then a[num - 1] := false
+  else writeln('array doesn`t contain this element'); 
 end;
 
-procedure DoesContain(a: mas; num: integer; N: integer);
-begin
-  if a[num - 1] = true then writeln('Array contains this element')
-  else writeln('Array doesn`t contain this element');
-end;
 
 begin
   writeln('Enter the size of array');
   read(N);
   setlength(a, N);
-  writeln('Enter the elment to add');
-  read(num1);
-  add(a, num1, n);
-  writeln('Enter the element to delete');
-  read(num2);
-  delete(a, num2, n);
-  writeln('Enter the element to know if the array contains it or not');
-  read(num3);
-  DoesContain(a, num3, n);
+  writeln('How many elements do you want to add?');
+  read(count1);
+  for i := 1 to count1 do
+  begin
+    writeln('Enter the elment to add');
+    read(num1);
+    add(a, num1, n);
+  end;
+  writeln('How many elements do you want to delete?');
+  read(count2);
+  for i := 1 to count2 do
+  begin
+    writeln('Enter the element to delete');
+    read(num2);
+    delete(a, num2, n);
+  end;
+  writeln('How many elements do you want to check?');
+  read(count3);
+  for i := 1 to count3 do
+  begin
+    writeln('Enter the element to know if the array contains it or not');
+    read(num3);
+    if DoesContain(a, num3, n) = true then writeln('Array contains this element')
+    else writeln('Array doesn`t contain this element');
+  end;
 end.
